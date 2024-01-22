4edf8e2b8a400c3be1777856a6279c68bd01c754
manjurulhasan <manjurul.hasan@selise.ch>
2024-01-22 22:37:49 +0600

    k8s file updated

--- undeploy.sh
+++ undeploy.sh
@@ -8,6 +8,6 @@ kubectl delete -n default configmap rupbot-nginx-config
 
 kubectl delete -n default service rupbot-service
 kubectl delete -n default deployment rupbot-deployment
-kubectl delete -n default persistentvolumeclaim rupbot-pvc
+# kubectl delete -n default persistentvolumeclaim rupbot-pvc
 
 echo "=========== delete success... ==========="
